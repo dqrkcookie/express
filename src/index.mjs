c1953f3376eb976a0be134d459000663a5e544d7
DarkCookie <crislester27@gmail.com>
2024-12-01 09:44:35 +0800

    w/id_parameter

--- src/index.mjs
+++ src/index.mjs
@@ -4,22 +4,36 @@ const app = express();
 
 const PORT = 4000;
 
+let users = [
+  {
+    id: 1,
+    name: 'Venti',
+    email: 'ventitheanemoarchon@gmail.com',
+    password: 'windguideyou',
+  },
+  {
+    id: 2,
+    name: 'Mona',
+    email: 'monamegistus@gmail.com',
+    password: 'hydro',
+  },
+];
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
 app.get('/users', (req, res) => {
-  let users = [
-    {
-      name: 'Venti',
-      email: 'ventitheanemoarchon@gmail.com',
-      password: 'windguideyou',
-    },
-    {
-      name: 'Mona',
-      email: 'monamegistus@gmail.com',
-      password: 'hydro',
-    },
-  ];
-  res.send(users[1]);
+  res.send(users);
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) return res.status(400).send('Invalid ID');
+
+  const find_user = users.find((user) => user.id === id);
+
+  if (find_user) return res.send(find_user);
+  return res.sendStatus(404);
 });
